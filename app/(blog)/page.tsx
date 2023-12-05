0b0573d52a837275929b085e9716bf7b702c624b
Travis Luong <travis@travisluong.com>
2023-12-05 12:18:36 +0800

    render latest post on home page

--- app/(blog)/page.tsx
+++ app/(blog)/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>Home</div>;
+import { getLatestPost } from "@/lib/api";
+import { DateTime } from "luxon";
+import Link from "next/link";
+
+export default async function Home() {
+  const post = await getLatestPost();
+  return (
+    <div>
+      <h2>
+        <Link href={`/${post.slug}`}>{post.title}</Link>
+      </h2>
+      <div>{post.date.toLocaleString(DateTime.DATETIME_FULL)}</div>
+      <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+    </div>
+  );
 }
