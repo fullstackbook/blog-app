f125d661eeeffcbc219389759bcc701d8b0de638
Travis Luong <travis@travisluong.com>
2023-12-05 12:25:10 +0800

    add markdown styles

--- app/(blog)/page.tsx
+++ app/(blog)/page.tsx
@@ -2,6 +2,7 @@ import { getLatestPost } from "@/lib/api";
 import { PostType } from "@/lib/types";
 import { DateTime } from "luxon";
 import Link from "next/link";
+import markdownStyles from "./markdown-styles.module.css";
 
 export default async function Home() {
   const post: PostType = await getLatestPost();
@@ -11,7 +12,10 @@ export default async function Home() {
         <Link href={`/${post.slug}`}>{post.title}</Link>
       </h2>
       <div>{post.date.toLocaleString(DateTime.DATETIME_FULL)}</div>
-      <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+      <div
+        className={markdownStyles["markdown"]}
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      ></div>
     </div>
   );
 }
